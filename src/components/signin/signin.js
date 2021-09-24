d235a569d00cb47a70187161a935b0bbdec55b2f
unknown <devjoytib@gmail.com>
2021-09-25 02:52:32 +0530

    added the signup api

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,8 +1,16 @@
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./signin.css";
 
 function Signin(){
+    
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    
+    const HandleSubmitButton=()=>{
+         
+         alert("hello")
+    }
 
     return(
 
@@ -12,12 +20,25 @@ function Signin(){
                <input 
                    type="text"
                    placeholder="email"
+                   value={email}
+                   minLength={5}
+                   maxLength={1000}
+                   required
+                   onChange={(e)=>setEmail(e.target.value)}
                 />
                 <input  
                    type="password"
                    placeholder="password"
+                   value={password}
+                   required
+                   minLength={5}
+                   maxLength={255}
+                   onChange={(e)=>setPassword(e.target.value)}
                 />
-                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                <button 
+                   className="btn waves-effect waves-light #64b5f6 blue lighten-2"
+                   onClick={()=>HandleSubmitButton()}
+                >
                     Signin
                 </button>
                 <h5> <Link to="/signup"> Dont't Have an account ? </Link></h5>
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,30 +1,73 @@
+import { useState } from 'react';
+import { Link,useHistory } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 
-import { Link } from 'react-router-dom';
-import './signup.css'
+import axios from 'axios';
+
+import './signup.css';
 function Signup(){
+    
+    const history=useHistory();
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const [confirmPassword,setConfirmPassword]=useState("");
+    
+
+    const HandleSubmit=async ()=>{
+         
+        try {
+
+            const data= await axios.post("http://localhost:5000/user/signup",{
+                name:name,
+                email:email,
+                password:password,
+                confirm_password:confirmPassword,
+    
+            })
+            alert("hello");
+            history.push("/login");
+            
+        } catch ({response}) {
+           
+           console.log(response);
+        }
+
+    }
 
     return(
 
         <div className="signup-container">
+            <Skeleton/>
             <div className="card signup-card">
                 <h2>Lopo</h2>
                 <input 
                     type="text"
                     placeholder="name"
+                    value={name}
+                    onChange={(e)=>{setName(e.target.value)}}
                  />
                 <input 
                     type="text"
                     placeholder="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                  />
                  <input  
                     type="password"
                     placeholder="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                  />
                 <input  
                     type="password"
                     placeholder="confirm-password"
+                    value={confirmPassword}
+                    onChange={(e)=>setConfirmPassword(e.target.value)}
                  />
-                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2"
+                     onClick={()=>HandleSubmit()}
+                 >
                      Signup
                  </button>
 
