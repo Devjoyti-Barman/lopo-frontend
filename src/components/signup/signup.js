036c539fa6e8a84608ea542b576e6df73f99559b
unknown <devjoytib@gmail.com>
2021-09-25 21:32:59 +0530

    added the loading screen in signup and react toast container

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { Link,useHistory } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 
 import './signup.css';
 function Signup(){
     
     const history=useHistory();
+    const [isLoading,setIsLoading]=useState(false);
+
     const [name,setName]=useState("");
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
@@ -15,22 +18,26 @@ function Signup(){
     
 
     const HandleSubmit=async ()=>{
+
+        setIsLoading(true);
+        
          
         try {
 
-            const data= await axios.post("http://localhost:5000/user/signup",{
+            const response= await axios.post("http://localhost:5000/user/signup",{
                 name:name,
                 email:email,
                 password:password,
                 confirm_password:confirmPassword,
     
             })
-            alert("hello");
-            history.push("/login");
+            toast.success(response.data.message);
+            setTimeout(()=>{history.push("/signin");},4000);
+            
             
         } catch ({response}) {
-           
-           console.log(response);
+           setIsLoading(false);
+           toast.error( response.data.error);
         }
 
     }
@@ -38,43 +45,49 @@ function Signup(){
     return(
 
         <div className="signup-container">
-            <Skeleton/>
-            <div className="card signup-card">
-                <h2>Lopo</h2>
-                <input 
-                    type="text"
-                    placeholder="name"
-                    value={name}
-                    onChange={(e)=>{setName(e.target.value)}}
-                 />
-                <input 
-                    type="text"
-                    placeholder="email"
-                    value={email}
-                    onChange={(e)=>setEmail(e.target.value)}
-                 />
-                 <input  
-                    type="password"
-                    placeholder="password"
-                    value={password}
-                    onChange={(e)=>setPassword(e.target.value)}
-                 />
-                <input  
-                    type="password"
-                    placeholder="confirm-password"
-                    value={confirmPassword}
-                    onChange={(e)=>setConfirmPassword(e.target.value)}
-                 />
-                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2"
-                     onClick={()=>HandleSubmit()}
-                 >
-                     Signup
-                 </button>
-
-                <h5>
-                    <Link to="/signin">All ready have an account ?</Link>
-                </h5>
-            </div>
+            <ToastContainer />
+            {
+                isLoading===true ? <Skeleton count={10}/> : <>
+            
+                <div className="card signup-card">
+                    <h2>Lopo</h2>
+                    <input 
+                        type="text"
+                        placeholder="name"
+                        value={name}
+                        onChange={(e)=>{setName(e.target.value)}}
+                     />
+                    <input 
+                        type="text"
+                        placeholder="email"
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
+                     />
+                     <input  
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                     />
+                    <input  
+                        type="password"
+                        placeholder="confirm-password"
+                        value={confirmPassword}
+                        onChange={(e)=>setConfirmPassword(e.target.value)}
+                     />
+                     <button className="btn waves-effect waves-light #64b5f6 blue lighten-2"
+                         onClick={()=>HandleSubmit()}
+                     >
+                         Signup
+                     </button>
+    
+                    <h5>
+                        <Link to="/signin">All ready have an account ?</Link>
+                    </h5>
+                </div>
+                </>
+            
+            }  
         </div>
 
     )
